d679798dd007dfa0038213f034ec70fe5b363a2b
Dowon <oplolpz@gmail.com>
2021-02-15 23:46:11 -0500

    Initial Commit

---
+++ SocketApp/SocketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketAppApp.swift
+//  SocketApp
+//
+//  Created by Dowon on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SocketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
